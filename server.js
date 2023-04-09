c9eea37fdd136cbc787cd7ec211f7c0827e82b94
barakaganai <barakaganai01@gmail.com>
2023-04-09 18:29:56 +0300

    initialize the app, connect to mongoDB and listen

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("test");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+//mongoose connection
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("connected to mongoDB");
+} catch (error) {
+  console.log(error);
+}
+
+app.listen(5000, () => {
+  console.log("server is listening and here we go!..");
+});
