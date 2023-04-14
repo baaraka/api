95ad0193bad4ea49b55195a9a37c4ae307ca0465
barakaganai <barakaganai01@gmail.com>
2023-04-14 11:49:50 +0300

    import authRoute and enable the express take input

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,9 @@ try {
   console.log(error);
 }
 
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(5000, () => {
