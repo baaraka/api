49cda735da49ce5ced94361ae87d4f87f7fe8bde
barakaganai <barakaganai01@gmail.com>
2023-04-14 12:35:58 +0300

    create login in auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -16,3 +16,20 @@ export const register = async (req, res) => {
       .send("Sorry, something went wrong. Please try again later.");
   }
 };
+
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User not found");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(400).send("Wrong password or username");
+
+    const { password, ...info } = user._doc;
+    res.status(200).send(info);
+  } catch (error) {
+    res
+      .status(500)
+      .send("Sorry, something went wrong. Please try again later.");
+  }
+};
