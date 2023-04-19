09616f74449ac5406437da58d1ccf32a196db8f5
barakaganai <barakaganai01@gmail.com>
2023-04-19 17:04:46 +0300

    change localhost and port number

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ try {
   console.log(error);
 }
 //middleware
-app.use(cors({ origin: "https://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
